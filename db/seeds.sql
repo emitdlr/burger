c8360c2a3a0aa0ae145a95920ac2c07c305ff62d
emitdlr <emit@utexas.edu>
2018-05-08 17:29:56 -0500

    Heroku - I hate it

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,8 +3,4 @@
 INSERT INTO burgers(burger_name, devoured, date)
 VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
   ("Whooper", false, "2016-11-05 04:20:01"),
-  ("Cheesyburger", false, "2016-11-05 04:20:02"),
-  ("Baconator", false, "2016-11-05 16:20:00"),
-  ("Quarter Pounder", false, "2016-11-05 16:20:01"),
-  ("Royal with Cheese", false, "2016-11-05 04:20:20"),
-  ("Super Double ", true, "2016-11-05 16:20:16");
+  ("Cheesyburger", false, "2016-11-05 04:20:02")
--- model/burger.js
+++ model/burger.js
@@ -2,7 +2,7 @@
 var orm = require('../config/orm.js');
 
 
-// create the code that will call the ORM functions using burger specific input for the ORM.
+//Call the ORM functions using burger specific input for the ORM.
 var burger = {
 
   selectAll: function(callback){
